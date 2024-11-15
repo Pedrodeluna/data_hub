dd4a179ba37a84f6c92fceb00bf24bcf73f54bb1
Shiyi <scheng1@upv.edu.es>
2024-11-15 17:27:46 +0100

    boton con animación de avion

--- script.js
+++ script.js
@@ -455,4 +455,107 @@ document.addEventListener('DOMContentLoaded', function () {
         loadExampleConversation();
         loadExampleProducts();
     });
+
+    document.getElementById('send-button').addEventListener('click', function (e) {
+        if (!this.classList.contains('active') && !this.disabled) {
+            this.classList.add('active');
+
+            gsap.to(this, {
+                keyframes: [{
+                    '--left-wing-first-x': 50,
+                    '--left-wing-first-y': 100,
+                    '--right-wing-second-x': 50,
+                    '--right-wing-second-y': 100,
+                    duration: .2,
+                    onComplete() {
+                        gsap.set(this, {
+                            '--left-wing-first-y': 0,
+                            '--left-wing-second-x': 40,
+                            '--left-wing-second-y': 100,
+                            '--left-wing-third-x': 0,
+                            '--left-wing-third-y': 100,
+                            '--left-body-third-x': 40,
+                            '--right-wing-first-x': 50,
+                            '--right-wing-first-y': 0,
+                            '--right-wing-second-x': 60,
+                            '--right-wing-second-y': 100,
+                            '--right-wing-third-x': 100,
+                            '--right-wing-third-y': 100,
+                            '--right-body-third-x': 60
+                        })
+                    }
+                }, {
+                    '--left-wing-third-x': 20,
+                    '--left-wing-third-y': 90,
+                    '--left-wing-second-y': 90,
+                    '--left-body-third-y': 90,
+                    '--right-wing-third-x': 80,
+                    '--right-wing-third-y': 90,
+                    '--right-body-third-y': 90,
+                    '--right-wing-second-y': 90,
+                    duration: .2
+                }, {
+                    '--rotate': 50,
+                    '--left-wing-third-y': 95,
+                    '--left-wing-third-x': 27,
+                    '--right-body-third-x': 45,
+                    '--right-wing-second-x': 45,
+                    '--right-wing-third-x': 60,
+                    '--right-wing-third-y': 83,
+                    duration: .25
+                }, {
+                    '--rotate': 55,
+                    '--plane-x': -8,
+                    '--plane-y': 24,
+                    duration: .2
+                }, {
+                    '--rotate': 40,
+                    '--plane-x': 45,
+                    '--plane-y': -180,
+                    '--plane-opacity': 0,
+                    duration: .3,
+                    onComplete: () => {
+                        setTimeout(() => {
+                            this.removeAttribute('style');
+                            gsap.fromTo(this, {
+                                opacity: 0,
+                                y: -8
+                            }, {
+                                opacity: 1,
+                                y: 0,
+                                clearProps: true,
+                                duration: .3,
+                                onComplete: () => {
+                                    this.classList.remove('active');
+                                }
+                            })
+                        }, 2000)
+                    }
+                }]
+            });
+
+            gsap.to(this, {
+                keyframes: [{
+                    '--text-opacity': 0,
+                    '--border-radius': 0,
+                    '--left-wing-background': getComputedStyle(this).getPropertyValue('--primary-darkest'),
+                    '--right-wing-background': getComputedStyle(this).getPropertyValue('--primary-darkest'),
+                    duration: .1
+                }, {
+                    '--left-wing-background': getComputedStyle(this).getPropertyValue('--primary'),
+                    '--right-wing-background': getComputedStyle(this).getPropertyValue('--primary'),
+                    duration: .1
+                }, {
+                    '--left-body-background': getComputedStyle(this).getPropertyValue('--primary-dark'),
+                    '--right-body-background': getComputedStyle(this).getPropertyValue('--primary-darkest'),
+                    duration: .4
+                }, {
+                    '--success-opacity': 1,
+                    '--success-scale': 1,
+                    duration: .25,
+                    delay: .25
+                }]
+            });
+        }
+    });
 });
